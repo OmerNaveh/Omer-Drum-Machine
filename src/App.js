4a15a6c9d3fe9b897787b0d06fac73790e298cc1
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2021-11-28 16:42:45 +0200

    inserting first elements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
 
 function App() {
   return (
-    <div className="App">
-      hello
+    <div className="drumMachine" id="drum-machine">
+      <div id='display'>
+      </div>
+      <div className='drum-pad'>Q</div>
+      <div className='drum-pad'>W</div>
+      <div className='drum-pad'>E</div>
+      <div className='drum-pad'>A</div>
+      <div className='drum-pad'>S</div>
+      <div className='drum-pad'>D</div>
+      <div className='drum-pad'>Z</div>
+      <div className='drum-pad'>X</div>
+      <div className='drum-pad'>C</div>
     </div>
   );
 }

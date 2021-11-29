438ee99eaba72b376702872db2013c865415b366
Omer Naveh <89572395+OmerNaveh@users.noreply.github.com>
2021-11-29 14:26:31 +0200

    linked sounds to drumpads and added click and keypress

--- src/Components/DrumPad.jsx
+++ src/Components/DrumPad.jsx
@@ -1,8 +1,22 @@
+import { useRef } from "react";
 
 export default function DrumPad(props){
+    const audio = useRef(null)
+    document.addEventListener('keydown',(e)=>{
+        try{
+            if(e.key === props.text.toLowerCase()){playSound();}
+        }
+        catch{
+            return
+        }
+    })
+    const playSound = ()=>{
+       audio.current.play();
+    }
     return(
-        <div className="drum-pad" id={props.id}>
+        <div onClick={()=>playSound()} className="drum-pad" id={props.id}>
             {props.text}
+            <audio ref={audio} className="clip" id= {props.text} src={props.sound}></audio>
         </div>
     )
 }
